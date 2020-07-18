aebf306a311a06c80551b690eeece277515190ae
Chris Ashman <chrisashman96@gmail.com>
2020-07-18 00:14:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function loadImages() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-  fetch(imgUrl)
-    .then(res=> res.json())
-    .then(results => {
+  fetch(imgUrl).then(res=> res.json()).then(results => {
       results.message.forEach(image => addImage(image))
     });
 
